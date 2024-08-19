bc6c0c79f8e7a21e6d0cb6f554485ebd46b1e588
Aviral Singh <aviralsingh360@gmail.com>
2024-08-19 15:09:39 +0530

    First Commit

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,29 @@
+import { PerspectiveCamera, View } from "@react-three/drei";
+import React, { Suspense } from "react";
+import Lights from "./Lights";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  <View
+    index={index}
+    id={gsapType}
+    className={`border-2 border-red-500 w-full h-full ${
+      index === 2
+    }?'right-[-100%]:''`}
+  >
+    {/*Ambient Light*/}
+    <ambientLight intensity={0.3} />
+    <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+    <Lights />
+    <Suspense fallback={<div>Loading..</div>}></Suspense>
+  </View>;
+};
+
+export default ModelView;
